40560b5c5aa51120146a2fa1d2854a98b855c7bb
Bryna Stern <bryna.stern@gmail.com>
2023-09-26 18:54:14 -0400

    added a toString() method

--- src/code/HistoryItem.java
+++ src/code/HistoryItem.java
@@ -11,4 +11,11 @@ public class HistoryItem {
 	public int getAmount() {
 		return amount;
 	}
+	@Override
+	public String toString() 
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append("Amount: "+ amount);
+		return sb.toString();
+	}
 }
